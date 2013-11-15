1484be54fe0a4983cd60a4c30ded5b5679da70d0
alexsoble <asoble@gmail.com>
2013-11-15 08:23:08 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodScraper::Application.config.secret_token = '72921cb48140181c1a96686dfda2c1a296938d5dcf2a9981269d4d347330022e211e135cbe4b16000ddc3fe318f9f4f720cbfd9c2bdbc845225aa079f58a2b8c'
