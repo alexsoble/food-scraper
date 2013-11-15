7b95e079b2c23a2b893737b8c74f3b784a88441b
alexsoble <asoble@gmail.com>
2013-11-15 09:21:21 -0600

    Weird copy paste junk got in my code... OUT OUT

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,10 @@
 
 class UserMailer < ActionMailer::Base
   default from: "new-restaurants@foodscraper.com"
-
+  
   def scraping_email
-    @url = 'http://foodscraper.com',
-    mail(to: "asoble@gmail.com",  subject: 'Repot: New restaurants in Chicago')
+    @url = "foodscraper.com"
+    mail(to: "asoble@gmail.com", subject: "REPORT")
   end
+
 end
