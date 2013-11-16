bfed07440c2f8595ba37c54ffb340b298ff1defa
alexsoble <asoble@gmail.com>
2013-11-15 21:09:32 -0600

    grr typos

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,8 +8,8 @@ class UserMailer < ActionMailer::Base
 
     @new_restaurants = Restaurant.where(:sent => false)
     @yelp_restaurants = Restaurant.where(:source => "Yelp", :sent => false)
-    @city_restaurants_food = Restaurant.where(:source => "City-Food" :sent => false)
-    @city_restaurants_liquor = Restaurant.where(:source => "City-Liquor" :sent => false)
+    @city_restaurants_food = Restaurant.where(:source => "City-Food", :sent => false)
+    @city_restaurants_liquor = Restaurant.where(:source => "City-Liquor", :sent => false)
 
     @url = "foodscraper.com"
     mail(to: "asoble@gmail.com", subject: "Report: New restaurants in Chicago")
